1ae550bc050202d738e4ecf95607548ec0c0f398
SergeyL88 <Qwerty@3112311>
2025-01-13 21:13:51 +0400

    Обзор сторонних библиотек Python

--- module_11_1.py
+++ module_11_1.py
@@ -1,34 +1,39 @@
-from bs4 import BeautifulSoup as bs
 from simple_term_menu import TerminalMenu as tm
 import numpy
 import pandas as pd
 import requests
 
-menu_list: list = ['1) Requests/BS4', '2) Numpy', '3) Pandas']
-terminal_menu = tm(menu_list)
-menu_entry = terminal_menu.show()
-if menu_entry == 0:
-    # REQUESTS/BS4
 
-    URL = 'https://www.avito.ru'
-    HEADERS = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
-    }
-    try:
-        req = requests.get(url=URL, headers=HEADERS)
-    except ConnectionError:
-        print('Connection Error')
+if __name__ == "__main__":
+    while True:
+        menu_list: list = ['1) Numpy', '2) Pandas', '3) Requests', '4) Exit']
+        terminal_menu = tm(menu_list)
+        menu_entry = terminal_menu.show()
 
-    soup = bs(req.text)
-    print(soup)
-elif menu_entry == 1:
-    # Numpy
+        if menu_entry == 0:
+            # Numpy
 
-    array_1 = numpy.array([[5, 6, 7], [1, 2, 3]])
-    array_2 = numpy.array([[3, 7, 9], [4, 8, 2]])
-    print(array_1 + array_2)
-elif menu_entry == 2:
-    # Pandas
+            array_1 = numpy.array([[5, 6, 7], [1, 2, 3]])
+            array_2 = numpy.array([[3, 7, 9], [4, 8, 2]])
+            print(f'Сложение двумерных массивов:')
+            print(f'{array_1}\n  +\n{array_2}\n  =\n{array_1 + array_2}')
 
-    price_list = pd.read_csv('price_list.txt')
-    print(price_list)
+        elif menu_entry == 1:
+            # Pandas
+
+            price_list = pd.read_csv('price_list.txt')
+            print(price_list)
+        elif menu_entry == 2:
+            # Requests
+
+            site = input(f'Введите имя сайта для проверки:\n')
+            try:
+                req = requests.get(url=f'https://{site}', timeout=3)
+                print(f'Сайт https://{site} доступен')
+            except requests.exceptions.ConnectionError as e:
+                print(f'Проблема с подключением к сайту: https://{
+                      site}.\nПодробнее:\n{e}\n')
+
+        elif menu_entry == 3:
+            break
+    print("Работа программы завершена")
